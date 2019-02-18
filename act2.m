7fc5d9c9a06efa68e47b588edd07178b4af22219
Felix1663022 <47707918+Felix1663022@users.noreply.github.com>
2019-02-17 21:48:26 -0600

    Add files via upload

---
+++ act2.m
@@ -0,0 +1,21 @@
+%Ejercicio 1
+syms x a 
+solve('2*x+a==5', x)
+%Ejercicio 2
+syms x a b
+solve('x^2+a*b+b==0', x)
+%Ejercicio 3
+syms x
+solve('2*exp(x)+3*cos(x)==0', x)
+%Ejercicio 4
+syms c y x
+y1=2*x-3*c*y;
+y2=c*x+2*y;
+eqns = [y==5, y1==7];
+[sol4_1, sol4_2] = solve(eqns)
+%Ejercicio 5
+syms x y
+y1=3*x^2-2*x+y;
+y2=x*y+x;
+eqns = [y==7, y1==5];
+[sol4_1, sol4_2] = solve(eqns)
